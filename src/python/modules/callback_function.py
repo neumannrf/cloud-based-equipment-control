893bb9f6d4c08b7c36d95e0a160752294ae90ec5
Rodrigo Neumann <neumannrf@gmail.com>
2020-06-22 20:47:09 -0300

    Add JSON parsing to `myCommandCallback()`

--- src/python/modules/callback_function.py
+++ src/python/modules/callback_function.py
@@ -5,4 +5,39 @@ def myCommandCallback(cmd):
     print("\tCommandID: %s" % cmd.commandId)
     print("\tFormat: %s" % cmd.format)
 
-    print("\nAdd your custom commands here!\n")
+    # PicoHarp: define measurement parameters
+    binning = cmd.data['binning']               # you can change this
+    offset = cmd.data['offset']
+    tacq = cmd.data['tacq']                     # Measurement time in millisec, you can change this
+    syncDivider = cmd.data['syncDivider']       # you can change this
+    CFDZeroCross0 = cmd.data['CFDZeroCross0']   # you can change this (in mV)
+    CFDZeroCross1 = cmd.data['CFDZeroCross1']   # you can change this (in mV)
+    CFDLevel0 = cmd.data['CFDLevel0']           # you can change this (in mV)
+    CFDLevel1 = cmd.data['CFDLevel1']           # you can change this (in mV)
+
+    # ScontelControlUnit: define measurement parameters
+    detector = cmd.data['detector']     # select detector 0 or 1
+    Istart = cmd.data['Istart']         # in uA
+    Istop = cmd.data['Istop']           # in uA
+    Istep = cmd.data['Istep']           # in uA
+    average = cmd.data['average']       # time average in s
+    steps = int((Istop - Istart) / Istep) + 1
+
+    # Print measurement parameters
+    print('\n===================================')
+    print('Measurement parameters')
+    print('binning', binning)
+    print('offset', offset)
+    print('tacq', tacq)
+    print('syncDivider', syncDivider)
+    print('CFDZeroCross0', CFDZeroCross0)
+    print('CFDZeroCross1', CFDZeroCross1)
+    print('CFDLevel0', CFDLevel0)
+    print('CFDLevel1', CFDLevel1)
+    print('detector', detector)
+    print('Istart', Istart)
+    print('Istop', Istop)
+    print('Istep', Istep)
+    print('average', average)
+    print('steps', steps)
+    print('===================================\n')
