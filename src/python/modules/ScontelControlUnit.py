e38a82e05e0366be0b907619b06283eb86edd864
Rodrigo Neumann <neumannrf@gmail.com>
2020-07-09 19:44:47 -0300

    Create `ScontelControlUnit` class

---
+++ src/python/modules/ScontelControlUnit.py
@@ -0,0 +1,30 @@
+# import ctypes as ct
+
+
+class ScontelControlUnit:
+    """Class that stores ScontelControlUnit measurement parameters and functions."""
+
+    # Standard library
+    # culib = ct.WinDLL("C:\\ControlUnit Toolset\\biasbox.dll")
+
+    # Initialize class with parameter values
+    def __init__(self, detector, Istart, Istop, Istep, average):
+        self.Istep = Istep
+        self.Istop = Istop
+        self.Istart = Istart
+        self.average = average
+        self.detector = detector
+        self.steps = int((Istop - Istart) / Istep) + 1
+
+    # Helper function for printing logs
+    def __str__(self):
+        string = '\n===================================\n' + \
+                 'ScontelControlUnit measurement parameters:\n' + \
+                 'Detector = {0}\n'.format(self.detector) + \
+                 'Initial current = {0} uA\n'.format(self.Istart) + \
+                 'Final current = {0} uA\n'.format(self.Istop) + \
+                 'Current step = {0} uA\n'.format(self.Istep) + \
+                 'Time average = {0} s\n'.format(self.average) + \
+                 'Number of steps = {0}\n'.format(self.steps) + \
+                 '===================================\n'
+        return string
